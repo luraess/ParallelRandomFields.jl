3f1c60c3305116036927a6228a8ff6e40cb9cf2e
Ludovic Räss <ludovic.rass@gmail.com>
2021-03-17 15:22:21 +0100

    refactore docstrings

--- src/grf2D_CUDA.jl
+++ src/grf2D_CUDA.jl
@@ -17,9 +17,67 @@ module grf2D_CUDA
 
     export grf2D_expon!, grf2D_gauss!, generate_grf2D
 
-    @doc "`grf2D_expon!()`: Compute the Gaussian random field with anisotropic exponential covariance function." :(grf2D_expon!)
-    @doc "`grf2D_gauss!()`: Compute the Gaussian random field with isotropic Gaussian covariance function." :(grf2D_gauss!)
-    @doc "`generate_grf2D()`: Generate the Gaussian random field with chosen covariance function. Default (but modifiable) input arguments are: `cov_typ=\"expon\", do_viz=true, do_save=false, lx=100.0, ly=100.0, sf=1.0, cl=(10.0, 8.0), k_m=100.0, nh=10000, nx=64, ny=64.`" :(generate_grf2D)
+    @doc raw"""
+
+        grf2D_expon!(Yf, sf, cl, nh, nx, ny, dx, dy; do_reset=true)
+
+    Compute the Gaussian random field `Yf` with anisotropic exponential covariance function.
+
+    # Arguments
+    - `Yf::Data.Array`: the Gaussian random field array.
+    - `sf::Data.Number`: the standard deviation.
+    - `cl::Any`: the correlation length tuple `(clx, cly)` in `x` and `y` dimension.
+    - `nh::Int`: the number of harmonics.
+    - `nx::Int`: the number of grid points in `x` dimension.
+    - `ny::Int`: the number of grid points in `y` dimension.
+    - `dx::Data.Number`: the numerical grid size in `x` dimension.
+    - `dy::Data.Number`: the numerical grid size in `y` dimension.
+    - `do_reset=true`: the optional kwarg to reset the random seed.
+    """
+    grf2D_expon!()
+
+    @doc raw"""
+
+        grf2D_gauss!(Yf, sf, cl, nh, k_m, nx, ny, dx, dy; do_reset=true)
+
+    Compute the Gaussian random field `Yf` with isotropic Gaussian covariance function.
+
+    # Arguments
+    - `Yf::Data.Array`: the Gaussian random field array.
+    - `sf::Data.Number`: the standard deviation.
+    - `cl::Any`: the correlation length tuple `(clx, cly)` in `x` and `y` dimension.
+    - `nh::Int`: the number of harmonics.
+    - `k_m::Data.Number`: the maximum value of the wave number.
+    - `nx::Int`: the number of grid points in `x` dimension.
+    - `ny::Int`: the number of grid points in `y` dimension.
+    - `dx::Data.Number`: the numerical grid size in `x` dimension.
+    - `dy::Data.Number`: the numerical grid size in `y` dimension.
+    - `do_reset=true`: the optional kwarg to reset the random seed.
+    """
+    grf2D_gauss!()
+    
+    @doc raw"""
+
+        generate_grf2D(lx=100.0, ly=100.0, sf=1.0, cl=(10.0, 8.0), k_m=100.0, nh=10000, nx=64, ny=64;
+                       cov_typ=\"expon\", do_reset=true, do_viz=true, do_save=false)
+
+    Returns the Gaussian random field with exponential `cov_typ=\"expon\"` or Gaussian `cov_typ=\"gauss\"` covariance function for default values. 
+
+    # Arguments
+    - `lx=100.0`: the `x` dimension domain extend.
+    - `ly=100.0`: the `y` dimension domain extend.
+    - `sf=1.0`: the standard deviation.
+    - `cl=(10.0, 8.0)`: the correlation length; `(clx, cly)` tuple (`cov_typ=\"expon\"`), `cl[1]/2.0` (`cov_typ=\"gauss\"`).
+    - `k_m=100.0`: the maximum value of the wave number.
+    - `nh=10000`: the number of harmonics.
+    - `nx=64`: the numerical grid size in `x` dimension.
+    - `ny=64`: the numerical grid size in `y` dimension.
+    - `cov_typ=\"expon\"`: the covariance function `\"expon\"` or `\"gauss\"`.
+    - `do_viz=true`: the optional kwarg to enable visualisation.
+    - `do_save=false`: the optional kwarg to save the random field to disk.
+    - `do_reset=true`: the optional kwarg to reset the random seed.
+    """
+    generate_grf2D()
 
     using ParallelStencil
     using ParallelStencil.FiniteDifferences2D
--- src/grf3D_CUDA.jl
+++ src/grf3D_CUDA.jl
@@ -17,10 +17,74 @@ module grf3D_CUDA
 
     export grf3D_expon!, grf3D_gauss!, generate_grf3D
 
-    @doc "`grf3D_expon!()`: Compute the Gaussian random field with anisotropic exponential covariance function." :(grf3D_expon!)
-    @doc "`grf3D_gauss!()`: Compute the Gaussian random field with isotropic Gaussian covariance function." :(grf3D_gauss!)
-    @doc "`generate_grf3D()`: Generate the Gaussian random field with chosen covariance function. Default (but modifiable) input arguments are: `cov_typ=\"expon\", do_viz=true, do_save=false, lx=100.0, ly=100.0, lz=100.0, sf=1.0, cl=(10.0, 8.0, 5.0), k_m=100.0, nh=10000, nx=64, ny=64, nz=64.`" :(generate_grf3D)
+    @doc raw"""
 
+        grf3D_expon!(Yf, sf, cl, nh, nx, ny, nz, dx, dy, dz; do_reset=true)
+
+    Compute the Gaussian random field `Yf` with anisotropic exponential covariance function.
+
+    # Arguments
+    - `Yf::Data.Array`: the Gaussian random field array.
+    - `sf::Data.Number`: the standard deviation.
+    - `cl::Any`: the correlation length tuple `(clx, cly, clz)` in `x`, `y` and `z` dimension.
+    - `nh::Int`: the number of harmonics.
+    - `nx::Int`: the number of grid points in `x` dimension.
+    - `ny::Int`: the number of grid points in `y` dimension.
+    - `nz::Int`: the number of grid points in `z` dimension.
+    - `dx::Data.Number`: the numerical grid size in `x` dimension.
+    - `dy::Data.Number`: the numerical grid size in `y` dimension.
+    - `dz::Data.Number`: the numerical grid size in `z` dimension.
+    - `do_reset=true`: the optional kwarg to reset the random seed.
+    """
+    grf3D_expon!()
+
+    @doc raw"""
+
+        grf3D_gauss!(Yf, sf, cl, nh, k_m, nx, ny, nz, dx, dy, dz; do_reset=true)
+
+    Compute the Gaussian random field `Yf` with isotropic Gaussian covariance function.
+
+    # Arguments
+    - `Yf::Data.Array`: the Gaussian random field array.
+    - `sf::Data.Number`: the standard deviation.
+    - `cl::Any`: the correlation length tuple `(clx, cly, clz)` in `x`, `y` and `z` dimension.
+    - `nh::Int`: the number of harmonics.
+    - `k_m::Data.Number`: the maximum value of the wave number.
+    - `nx::Int`: the number of grid points in `x` dimension.
+    - `ny::Int`: the number of grid points in `y` dimension.
+    - `nz::Int`: the number of grid points in `z` dimension.
+    - `dx::Data.Number`: the numerical grid size in `x` dimension.
+    - `dy::Data.Number`: the numerical grid size in `y` dimension.
+    - `dz::Data.Number`: the numerical grid size in `z` dimension.
+    - `do_reset=true`: the optional kwarg to reset the random seed.
+    """
+    grf3D_gauss!()
+    
+    @doc raw"""
+
+        generate_grf3D(lx=100.0, ly=100.0, lz=100.0, sf=1.0, cl=(10.0, 8.0, 5.0), k_m=100.0, nh=10000, nx=64, ny=64, nz=64;
+                       cov_typ=\"expon\", do_reset=true, do_viz=true, do_save=false)
+
+    Returns the Gaussian random field with exponential `cov_typ=\"expon\"` or Gaussian `cov_typ=\"gauss\"` covariance function for default values. 
+
+    # Arguments
+    - `lx=100.0`: the `x` dimension domain extend.
+    - `ly=100.0`: the `y` dimension domain extend.
+    - `lz=100.0`: the `z` dimension domain extend.
+    - `sf=1.0`: the standard deviation.
+    - `cl=(10.0, 8.0, 5.0)`: the correlation length; `(clx, cly, clz)` tuple (`cov_typ=\"expon\"`), `cl[1]/2.0` (`cov_typ=\"gauss\"`).
+    - `k_m=100.0`: the maximum value of the wave number.
+    - `nh=10000`: the number of harmonics.
+    - `nx=64`: the numerical grid size in `x` dimension.
+    - `ny=64`: the numerical grid size in `y` dimension.
+    - `nz=64`: the numerical grid size in `y` dimension.
+    - `cov_typ=\"expon\"`: the covariance function `\"expon\"` or `\"gauss\"`.
+    - `do_viz=true`: the optional kwarg to enable visualisation.
+    - `do_save=false`: the optional kwarg to save the random field to disk.
+    - `do_reset=true`: the optional kwarg to reset the random seed.
+    """
+    generate_grf3D()
+    
     using ParallelStencil
     using ParallelStencil.FiniteDifferences3D
     @init_parallel_stencil(CUDA, Float64, 3)
